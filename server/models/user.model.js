0f5667bb9f125e3f26916fd22d16c07bd2f4f717
Dang Hoang Ha <ha.dh290702@gmail.com>
2024-06-20 20:16:04 +0300

    Fix duplicate fullName, implement 1st part UserRoutes && Controllers

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -10,7 +10,6 @@ const userSchema = new mongoose.Schema(
                 fullName: {
                         type: String,
                         require: true,
-                        unique: true,
                 },
                 password: {
                         type: String,
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.routes.js';
-import connectMongoDb from './db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
+
+import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
+import connectMongoDb from './db/connectMongoDB.js';
 
 dotenv.config();
 const app = express();
@@ -12,6 +14,7 @@ app.use(express.urlencoded({ extended: true })); //To parse form data
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 // Add a route for the root URL
 app.get('/', (req, res) => {
